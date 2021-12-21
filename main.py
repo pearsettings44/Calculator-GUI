653d98e0d6b28765f487882e98cddeba09e6a7c1
pearsettings44 <jose.a.pereira03@gmail.com>
2021-12-21 20:37:13 +0000

    Added functionality to the calculator. Missing square and square root functions.

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ class Calculator():
         self.window.resizable(0, 0)
         self.window.title = ("Calculator")
 
-        self.result_expression = "0"
-        self.current_expression = "0"
+        self.result_expression = ""
+        self.current_expression = ""
 
         self.display_frame = self.create_display_frame()
         self.result_label, self.current_label = self.create_display_labels()
@@ -80,14 +80,32 @@ class Calculator():
         frame.pack(expand=True, fill="both")
         return frame
 
+    def add_to_expression(self, value):
+        """
+        Adds value to current expression.
+        """
+        self.current_expression += str(value)
+        self.update_current_label()
+
     def create_digits_buttons(self):
         """
         Creates the digits (1,...,9) buttons.
         """
         for digit, grid_value in self.digits.items():
             button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE,
-                            fg=LABEL_COLOR, font=DIGIT_FONT_STYLE, borderwidth=0)
+                            fg=LABEL_COLOR, font=DIGIT_FONT_STYLE, borderwidth=0,
+                            command=lambda x=digit: self.add_to_expression(x))
             button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def append_operator(self, operator):
+        """
+        Adds the operator
+        """
+        self.current_expression += operator
+        self.result_expression += self.current_expression
+        self.current_expression = ""
+        self.update_current_label()
+        self.update_result_label()
 
     def create_operator_buttons(self):
         """
@@ -96,24 +114,47 @@ class Calculator():
         i = 0
         for operator, symbol in self.operations.items():
             button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE,
-                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,
+                            command=lambda x=operator: self.append_operator(x))
             button.grid(row=i, column=4, sticky=tk.NSEW)
             i += 1
+
+    def clear(self):
+        """
+        Clears the current and the result expressions.
+        """
+        self.current_expression = ""
+        self.result_expression = ""
+        self.update_result_label()
+        self.update_current_label()
 
     def create_clear_button(self):
         """
         Creates the clear button.
         """
         button = tk.Button(self.buttons_frame, text="C", bg=OFF_WHITE,
-                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,
+                            command=self.clear)
         button.grid(row=0, column=1, columnspan=3 ,sticky=tk.NSEW)
+
+    def evalute(self):
+        """
+        Uses eval to get the result.
+        """
+        self.result_expression += self.current_expression
+        self.update_result_label
+
+        self.current_expression = str(eval(self.result_expression))
+        self.result_expression = ""
+        self.update_current_label()
 
     def create_equal_button(self):
         """
         Creates the equal button
         """
         button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE,
-                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+                            fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,
+                            command=self.evalute)
         button.grid(row=4, column=3, columnspan=2 ,sticky=tk.NSEW)
 
     def create_buttons_frame(self):
@@ -123,6 +164,18 @@ class Calculator():
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill="both")
         return frame
+
+    def update_result_label(self):
+        """
+        Updates the result label
+        """
+        self.result_label.config(text=self.result_expression)
+
+    def update_current_label(self):
+        """
+        Updates the current label
+        """
+        self.current_label.config(text=self.current_expression)
 
     def run(self):
         """
